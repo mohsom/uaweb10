969aed51a19512360eca91e10baeedaf50b79bc6
mohsom <volodya.skalskyi@gmail.com>
2016-09-05 19:54:58 +0300

    Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,178 +1,173 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'build/index.html': 'index.html'
-                }
-            }
-        },
-        uglify: {
+import grunt from 'grunt';
+import loadTasks from 'load-grunt-tasks';
+loadTasks(grunt);
+grunt.initConfig({
+    htmlmin: {
+        dist: {
             options: {
-                mangle: false
+                removeComments: true,
+                collapseWhitespace: true
             },
-            my_target: {
-                files: {
-                    'build/js/script.js': [
-                        'js/script.js'
-                    ],
-                    'build/js/offline-disable.js': [
-                        'js/offline-disable.js'
-                    ]
-                }
-            }
-        },
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        src: [
-                            'bower_components/**'
-                        ],
-                        dest: 'build/'
-                    }
-                ]
-            }
-        },
-        cssmin: {
-            options: {
-                shorthandCompacting: false,
-                roundingPrecision: -1
-            },
-            target: {
-                files: {
-                    'build/styles/css/style.css': [
-                        'styles/css/style.css'
-                    ]
-                }
-            }
-        },
-
-        clean: {
-            build: ['build']
-        },
-        sass: {
-            dist: {
-                files: {
-                    'styles/css/style.css': 'styles/sass/style.scss'
-                }
-            }
-        },
-
-        jade: {
-            compile: {
-                options: {
-                    data: {
-                        debug: false
-                    }
-                },
-                files: {
-                    "index.html": ["jade/index.jade"]
-                }
-            }
-        },
-
-        connect: {
-
-            options: {
-                port: 3000,
-                hostname: 'localhost',
-                livereload: 35719
-            },
-
-            livereload: {
-                options: {
-                    open: true
-                }
-            }
-        },
-
-        watch: {
-            scss: {
-                files: ['styles/sass/*.scss'],
-                tasks: ['sass']
-                //options:{
-                //    livereload: '<%= connect.options.livereload %>',
-                //}
-            },
-            jade:{
-                files:['jade/*.jade'],
-                tasks: ['jade']
-            },
-            css: {
-                files: ['styles/css/*.css']
-            },
-            html: {
-                files: ['*.html']
-                //tasks: ['validation']
-            },
-            js: {
-                files: ['js/*.js', 'Gruntfile.js']
-            },
-            options: {
-                //livereload: '<%= connect.options.livereload %>'
-            },
-            livereload: {
-                files: [
-                    'styles/css/*.css',
-                    '<%=watch.html.files%>',
-                    '<%=watch.js.files%>'
-                ],
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                }
-            }
-        },
-        validation: {
             files: {
-                src: ['*.html']
-            }
-        },
-        scsslint: {
-            allFiles: [
-                'styles/sass/*.scss'
-            ],
-            options: {
-                bundleExec: true,
-                config: '.scss-lint.yml',
-                reporterOutput: 'scss-lint-report.xml',
-                colorizeOutput: true
-            }
-        },
-        imagemin: {
-            dynamic: {
-                options: {
-                    optimizationLevel: 7
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,jpeg}'],
-                    dest: 'build/img'
-                }]
+                'build/index.html': 'index.html'
             }
         }
-    });
+    },
+    uglify: {
+        options: {
+            mangle: false
+        },
+        my_target: {
+            files: {
+                'build/js/script.js': [
+                    'js/script.js'
+                ],
+                'build/js/offline-disable.js': [
+                    'js/offline-disable.js'
+                ]
+            }
+        }
+    },
+    copy: {
+        main: {
+            files: [
+                {
+                    expand: true,
+                    src: [
+                        'bower_components/**'
+                    ],
+                    dest: 'build/'
+                }
+            ]
+        }
+    },
+    cssmin: {
+        options: {
+            shorthandCompacting: false,
+            roundingPrecision: -1
+        },
+        target: {
+            files: {
+                'build/styles/css/style.css': [
+                    'styles/css/style.css'
+                ]
+            }
+        }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-scss-lint');
-    grunt.loadNpmTasks('grunt-html-validation');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.registerTask('build', ['clean:build', 'sass','jade', 'cssmin', 'htmlmin' ,'copy','uglify', 'imagemin']);
-    grunt.registerTask('serve', ['jade','sass', 'connect', 'watch']);
-    grunt.registerTask('valid', ['validation','scsslint']);
-};
+    clean: {
+        build: ['build']
+    },
+    sass: {
+        dist: {
+            files: {
+                'styles/css/style.css': 'styles/sass/style.scss'
+            }
+        }
+    },
+
+    jade: {
+        compile: {
+            options: {
+                data: {
+                    debug: false
+                }
+            },
+            files: {
+                "index.html": ["jade/index.jade"]
+            }
+        }
+    },
+
+    connect: {
+
+        options: {
+            port: 3000,
+            hostname: 'localhost',
+            livereload: 35719
+        },
+
+        livereload: {
+            options: {
+                open: true
+            }
+        }
+    },
+
+    watch: {
+        scss: {
+            files: ['styles/sass/*.scss'],
+            tasks: ['sass']
+            //options:{
+            //    livereload: '<%= connect.options.livereload %>',
+            //}
+        },
+        jade: {
+            files: ['jade/*.jade'],
+            tasks: ['jade']
+        },
+        js: {
+            files: ['js/es6/*.es6'],
+            tasks: ['babel']
+        },
+        options: {
+            //livereload: '<%= connect.options.livereload %>'
+        },
+        livereload: {
+            files: [
+                'styles/css/*.css',
+                '<%=watch.html.files%>',
+                '<%=watch.js.files%>'
+            ],
+            options: {
+                livereload: '<%= connect.options.livereload %>'
+            }
+        }
+    },
+    validation: {
+        files: {
+            src: ['*.html']
+        }
+    },
+
+    babel: {
+        options: {
+            sourceMap: true,
+            presets: ['es2015']
+        },
+        dist: {
+            files: {
+                'js/es5/script.js': 'js/es6/script.es6'
+            }
+        }
+    },
+
+    scsslint: {
+        allFiles: [
+            'styles/sass/*.scss'
+        ],
+        options: {
+            bundleExec: true,
+            config: '.scss-lint.yml',
+            reporterOutput: 'scss-lint-report.xml',
+            colorizeOutput: true
+        }
+    },
+    imagemin: {
+        dynamic: {
+            options: {
+                optimizationLevel: 7
+            },
+            files: [{
+                expand: true,
+                cwd: 'img/',
+                src: ['**/*.{png,jpg,jpeg}'],
+                dest: 'build/img'
+            }]
+        }
+    }
+});
+
+grunt.registerTask('build', ['clean:build', 'sass', 'jade', 'cssmin', 'htmlmin', 'copy', 'babel' ,'uglify', 'imagemin']);
+grunt.registerTask('serve', ['jade', 'sass', 'babel','connect', 'watch']);
+grunt.registerTask('valid', ['validation', 'scsslint']);
---
+++ js/es5/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+alert('hi!');
+//# sourceMappingURL=script.js.map
