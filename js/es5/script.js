5e0d726b7463ef982fc03ee1948653936bb50fe5
mohsom <volodya.skalskyi@gmail.com>
2016-09-17 17:18:09 +0300

    Commit

--- js/es5/script.js
+++ js/es5/script.js
@@ -1,13 +1,2 @@
 'use strict';
-
-var header = document.querySelector('.header');
-window.onscroll = function () {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrolled > 150) {
-    var className = header.getAttribute('class') + ' scrolled';
-    header.setAttribute('class', className);
-  } else {
-    var _className = header.getAttribute('class') - 'scrolled';
-  }
-};
 //# sourceMappingURL=script.js.map
